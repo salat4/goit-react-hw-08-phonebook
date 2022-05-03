f86c910f04ab2dc0da62eba5cba955781e3faf39
Dima Malik <mdima4266@gmail.com>
2022-05-03 22:40:25 +0200

    add in app all function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,52 @@
 import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
+import React, { useState , useEffect } from 'react';
+import Notiflix from "notiflix";
+import { nanoid } from "nanoid";
+
 export const App = () =>  {
+  const loginInputId = nanoid();
+  const LS_KEY = "reader_contact";
+  const [contacts, setContacts] = useState(()=>{
+    const saved = localStorage.getItem(LS_KEY);
+    const initialValue = JSON.parse(saved);
+    return initialValue || [];
+  });
+  const [filter, setFilter] = useState("")
+
+  useEffect(() => {
+    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  },[contacts])
+  const saveChange = (e) => {
+    e.preventDefault()
+    for (let i = 0; i < contacts.length; i++){
+      if (contacts[i].name === e.target.elements.name.value) {
+        return Notiflix.Notify.info(`${e.target.elements.name.value} is already is contacts`);
+      }
+    }  
+    setContacts([...contacts,{id : loginInputId,name : e.target.elements.name.value , number : e.target.elements.number.value}])
+}
+  const handelFilter = (e) => {
+  setFilter(e.target.value)
+} 
+  const deleteContacts = (e) => {
+    let deleteCont = [...contacts]
+    for (let i = 0; i < contacts.length; i++){
+        if (e.target.id === contacts[i].id) {
+         deleteCont.splice(i, 1);
+      } 
+    }
+    setContacts(deleteCont)
+  }  
+  
     return (
       <div>
         <h1>PhoneBook</h1>
-        <ContactForm/>
+        <ContactForm saveChange ={saveChange}/>
+        <h2>Contacts</h2>
+      <Filter filter={filter} handelFilter={handelFilter}/>
+      <ContactList contacts={contacts} filter={filter} deleteContacts={ deleteContacts}/>
       </div>
     );
   }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,68 +1,13 @@
-import React, { useState , useEffect } from 'react';
-import Notiflix from "notiflix";
-import ContactList from "../ContactList/ContactList";
-import Filter from "../Filter/Filter";
-import { nanoid } from "nanoid";
 
-const ContactForm = () => {
-  const loginInputId = nanoid();
-  const LS_KEY = "reader_contact";
-  const [contacts, setContacts] = useState(()=>{
-    const saved = localStorage.getItem(LS_KEY);
-    const initialValue = JSON.parse(saved);
-    return initialValue || [];
-  });
-  const [name, setName] = useState('');
-  const [tel, setTel] = useState('');
-  const [filter, setFilter] = useState("")
-
-
-  useEffect(() => {
-    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
-  },[contacts])
-
-  const handelChangeName = (e) => {
-    setName(e.target.value)
-  }
-  const handelChangeTel = (e) => {
-    setTel(e.target.value)
-  }
-  const saveChange = () => {
-    if (name === "" || tel === "") {
-    return Notiflix.Notify.warning("Write name or number");
-    }
-    for (let i = 0; i < contacts.length; i++){
-      if (contacts[i].name === name) {
-        return Notiflix.Notify.info(`${name} is already is contacts`);
-      }
-    }  
-    setContacts([...contacts,{id : loginInputId,name : name , number : tel}])
-    setName("")
-    setTel("")
-}
-  const handelFilter = (e) => {
-  setFilter(e.target.value)
-} 
-  const deleteContacts = (e) => {
-    let deleteCont = [...contacts]
-    for (let i = 0; i < contacts.length; i++){
-        if (e.target.id === contacts[i].id) {
-         deleteCont.splice(i, 1);
-      } 
-    }
-    setContacts(deleteCont)
-  }  
-  
+const ContactForm = ({saveChange}) => {
   return (
     <div>
-    <form>
+    <form onSubmit={saveChange}>
     <label>
       Name
       <input
         type="text"
-        name="name"
-        value={name}
-        onChange={handelChangeName}
+        name="name"     
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
@@ -73,20 +18,16 @@ const ContactForm = () => {
       <input
         type="tel"
         name="number"
-        value={tel}
-        onChange={handelChangeTel}
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
       />
     </label>
-    <button type="button" onClick={saveChange}>
+    <button type="submit">
       Add contact
     </button>
       </form>
-      <h2>Contacts</h2>
-      <Filter filter={filter} handelFilter={handelFilter}/>
-      <ContactList contacts={contacts} filter={filter} deleteContacts={ deleteContacts}/>
+      
     </div>
 )}
   
